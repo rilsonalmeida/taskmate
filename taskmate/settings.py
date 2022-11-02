6ecb4109bf5e4792bf1e2e794bdc538fd21d6a05
Rilson Almeida <rilson.almeida@gmail.com>
2022-11-02 15:33:20 -0300

    Project updated-installed Django-Heroku

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -1,3 +1,4 @@
+import django-heroku
 import os
 import environ 
 from pathlib import Path
@@ -127,3 +128,5 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'todolist'
 
 LOGIN_URL = 'login'
+
+django_heroku.settings(locals())
